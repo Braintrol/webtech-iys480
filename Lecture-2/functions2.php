c1cad37887739ce0f9815003a25ae10de61d3581
amantrip <am4227@columbia.edu>
2016-06-09 22:21:25 -0400

    Added LEcture 4

--- Lecture-2/functions2.php
+++ Lecture-2/functions2.php
@@ -1,16 +1,19 @@
 <?php
+
 $dbhost = "localhost";	
 $dbuser = "root";
 $dbpass = "root";
-$dbname = "myDB";
+$dbname = "test";
+
+$conn = connect_db();
+echo "okay";
 
 function connect_db(){
 	$conn = mysqli_connect( $GLOBALS['dbhost'], $GLOBALS['dbuser'], $GLOBALS['dbpass'], $GLOBALS['dbname']);
-	if( mysqli_connect_errno($conn)){
-	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
+   if ( mysqli_connect_errno($conn) ) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
+   return $conn;
+}
 
-	return $conn;
-}	
-
-?>	
+?>
